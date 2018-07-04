1c2c3c66dcfcc8852c73f8945c0cbabac650e388
Sarah Jacob <smjacob91@gmail.com>
2018-07-04 15:53:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+  range = (2...105558).to_a
+  if range.include?(num)
+    num_index = range.index(num)
+    range.delete_at(num_index)
+  end
+  new_range = []
+  range.each do |other_number|
+      new_range.push(num % other_number)
+  end
+  if new_range.include?(0) || num <= 1
+    return false
+  else
+    return true
+  end
+end
